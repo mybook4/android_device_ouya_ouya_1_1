5991d4929c927299c09b5a93d5664eed8b187948
vinny75 <admin@vin.net>
2013-06-09 10:55:17 +0200

    updated vendor files

--- cm.mk
+++ cm.mk
@@ -1,12 +1,6 @@
-# Boot animation
+# Resolution values for bootanimation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-
-## Specify phone tech before including full_phone
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := ouya_1_1
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
@@ -14,9 +8,16 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration
 $(call inherit-product, device/ouya/ouya_1_1/full_ouya_1_1.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ouya_1_1
+#
+# Setup device specific product configuration.
+#
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := cm_ouya_1_1
 PRODUCT_BRAND := OUYA
+PRODUCT_DEVICE := ouya_1_1
 PRODUCT_MODEL := OUYA Console
 PRODUCT_MANUFACTURER := OUYA
+
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := ouya_1_1
